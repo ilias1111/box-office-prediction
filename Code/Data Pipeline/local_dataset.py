8bd45119fc672fe7b78ceb235331be5c759fdb71
Ilias Xenogiannis <ilias1111@users.noreply.github.com>
2022-03-30 18:48:55 +0300

    Pipeline

---
+++ Code/Data Pipeline/local_dataset.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import json
+from set_up_connections import *
+import pandas_gbq
+
+
+def get_data_from_bigquery(credentials_file):
+
+    """
+    Get data from BigQuery
+    """
+
+    credentials, project_id = get_gbq_credentials()
+    with open('./queries.json') as f:
+        queries = json.load(f)
+
+    for key, value in queries.items():
+        df = pd.read_gbq(value, project_id=project_id, credentials=credentials)
+        df.to_csv(f'./Data/{key}.csv', index=False)
+
+
+get_data_from_bigquery('./credentials.json')
