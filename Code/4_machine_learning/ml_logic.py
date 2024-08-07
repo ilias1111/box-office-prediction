98fbf5b77d1f30d17b1396efbb2c1e95c8a963bb
Ilias Xenogiannis <ilias1111@users.noreply.github.com>
2024-08-07 14:16:44 +0300

    Data

--- Code/4_machine_learning/ml_logic.py
+++ Code/4_machine_learning/ml_logic.py
@@ -83,7 +83,6 @@ class MOTR:
         elif task_type == 'regression':
             return {
                 # "dummy_regressor" : DummyRegressor(strategy='mean'),
-                # # "linear_regression": LinearRegression(n_jobs=-1),
                 # "random_forest_regressor": RandomForestRegressor(random_state=42, n_jobs=-1),
                 # "decision_tree_regressor": DecisionTreeRegressor(random_state=42),
                 # "xgboost_regressor": XGBRegressor(random_state=42, n_jobs=-1),
@@ -214,7 +213,7 @@ class MOTR:
             'preprocessor__numerical__scaler': [scaler_mapping[scaler] for scaler in param_grid.get('preprocessor__numerical__scaler',["StandardScaler"])]
         }
         if self.grid_type == 'random_search':
-            model_with_parameters, number_of_combinations = perform_random_search(model, model_name, X_train, y_train, cv=4, n_iter=30, scoring=self.select_scoring(), random_state=42, task_type = self.task_type)
+            model_with_parameters, number_of_combinations = perform_random_search(model, model_name, X_train, y_train, cv=5, n_iter=100, scoring=self.select_scoring(), random_state=42, task_type = self.task_type)
         elif self.grid_type != 'non_grid':
             number_of_combinations = len(list(ParameterGrid(param_grid)))
             model_with_parameters = GridSearchCV(model, param_grid, cv=5, scoring=self.select_scoring(), n_jobs=-1, verbose=0, pre_dispatch='4*n_jobs', error_score='raise')
@@ -446,7 +445,7 @@ if __name__ == "__main__":
     ID_COLUMN_NAME = "movie_id"
 
     DATA_FILES_LIST = os.listdir("./data/ml_ready_data")
-    #DATA_FILES_LIST = [i for i in DATA_FILES_LIST if i.split("__")[1] == "regression"]
+    # DATA_FILES_LIST = [i for i in DATA_FILES_LIST if i.split("__")[1] == "binary_classification"]
     DATA_FILES_LIST = [
                        "full__regression__no_outliers__complex.csv",
                        "small_productions__regression__no_outliers__complex.csv",
